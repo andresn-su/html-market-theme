ed3e56d76fc63436da9972864956786a9b766a12
Andre Nascimento <andre.s.04.get.up@gmail.com>
2021-02-20 18:10:59 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple HTML Market Theme
+
+This is a very simple market theme, built with MaterializeCSS and jQuery.
---
+++ script.js
@@ -0,0 +1,15 @@
+// Sidenav
+$(document).ready(() => {
+    $('.sidenav').sidenav()
+})
+
+// Carousel
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true
+});
+
+
+// Collapsible
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
